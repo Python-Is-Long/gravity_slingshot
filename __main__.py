6ec43665b887254d782657a8754a0f31e97824ad
seancg-68 <39596057+seancg-68@users.noreply.github.com>
2023-01-31 01:59:17 -0500

    slingshot movement

--- __main__.py
+++ __main__.py
@@ -17,50 +17,54 @@ SPRITE_SCALING = 0.5
 # How fast the camera pans to the player. 1.0 is instant.
 CAMERA_SPEED = 0.1
 # How fast the character moves
-PLAYER_MOVEMENT_SPEED = 7
+PLAYER_MOVEMENT_SPEED = 0.1
+G=0.1
 
 
-class Bullet(arcade.Sprite):
-    def __init__(self, image_file=None, scale=1.0, shadertoy=None, player_index=0):
+class MassBody(arcade.Sprite):
+    def __init__(self, image_file: str = None, scale: float = 1.0, mass: float = 0, change_angle: float = 0):
         super().__init__(image_file, scale)
-        self.type = None
-        self.shadertoy = shadertoy
+        self.mass = mass
+        self.speed_vector = Vec2(0, 0)
+        self.change_speed_vector = Vec2(0, 0)
+        self.change_angle = change_angle
+
+    def update(self):
+        self.angle += self.change_angle
+        self.speed_vector += self.change_speed_vector
+        self.center_x += self.speed_vector[0]
+        self.center_y += self.speed_vector[1]
+        self.change_speed_vector = Vec2(0, 0)
+
+
+class Planet(MassBody):
+    def __init__(self, image_file=None, scale=1.0, mass=0, change_angle=0):
+        super().__init__(image_file=image_file, scale=scale, mass=mass, change_angle=change_angle)
+
+    def draw(self):
+        pass
+
+
+class Bullet(MassBody):
+    def __init__(self, image_file=None, scale=1.0, mass=0, player_index=0):
+        super().__init__(image_file=image_file, scale=scale, mass=mass)
         self.player_index = player_index
 
     def draw(self):
         pass
 
 
-class Player(arcade.Sprite):
-    """ Player class """
+class Coin(MassBody):
+    def __init__(self, image_file=None, scale=1.0, mass=0):
+        super().__init__(image_file=image_file, scale=scale, mass=mass)
 
-    def __init__(self, image, scale):
-        """ Set up the player """
-
-        # Call the parent init
-        super().__init__(image, scale)
-
-        # Create a variable to hold our speed. 'angle' is created by the parent
-        self.speed_vector = Vec2(0, 0)
-
-    def update(self):
-        # Convert angle in degrees to radians.
-        angle_rad = math.radians(self.angle)
-
-        # Rotate the ship
-        self.angle += self.change_angle
-
-        # Use math to find our change based on our speed and angle
-        self.center_x += -self.speed * math.sin(angle_rad)
-        self.center_y += self.speed * math.cos(angle_rad)
+    def draw(self):
+        pass
 
 
-class MassBody(arcade.Sprite):
-    def __init__(self, image_file=None, scale=1.0, shadertoy=None, player_index=0):
-        super().__init__(image_file, scale)
-        self.type = None
-        self.shadertoy = shadertoy
-        self.player_index = player_index
+class Player(MassBody):
+    def __init__(self, image_file=None, scale=1.0, mass=0):
+        super().__init__(image_file=image_file, scale=scale, mass=mass)
 
     def draw(self):
         pass
@@ -92,13 +96,12 @@ class GameView(arcade.View):
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList()
+        self.planet_list = arcade.SpriteList()
 
 
         # Set up the player
         self.score = 0
-        self.player_sprite = arcade.Sprite(":resources:images/space_shooter/playerShip1_orange.png",
-                                           SPRITE_SCALING)
+        self.player_sprite = Player(":resources:images/space_shooter/playerShip1_Orange.png", SPRITE_SCALING, mass=100)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
@@ -106,7 +109,7 @@ class GameView(arcade.View):
         for i in range(5):
 
             # Create the coin instance
-            coin = arcade.Sprite(":resources:images/items/coinGold.png", SPRITE_SCALING / 3)
+            coin = Coin(":resources:images/items/star.png", SPRITE_SCALING/3)
 
             # Position the coin
             coin.center_x = random.randrange(MAP_WIDTH)
@@ -120,13 +123,20 @@ class GameView(arcade.View):
         for x in range(200, 1650, 210):
             for y in range(0, 1600, 64):
                 # Randomly skip a box so the player can find a way through
-                if random.randrange(5) > 0:
-                    wall = arcade.Sprite(f":resources:images/space_shooter/meteorGrey_big{random.randint(1,4)}.png", SPRITE_SCALING)
-                    wall.center_x = x
-                    wall.center_y = y
-                    self.wall_list.append(wall)
+                if random.randrange(100) < 5:
+                    radius = random.uniform(1,3)
+                    mass = 1000*radius**2
+                    planet = Planet(
+                        image_file=f":resources:images/space_shooter/meteorGrey_big{random.randint(1,4)}.png",
+                        scale=SPRITE_SCALING*3,
+                        mass=mass,
+                        change_angle=random.normalvariate(0,1)
+                    )
+                    planet.center_x = x
+                    planet.center_y = y
+                    self.planet_list.append(planet)
 
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.planet_list)
 
         # Track the current state of what key is pressed
         self.left_pressed = False
@@ -156,8 +166,12 @@ class GameView(arcade.View):
     def last_mouse_y(self):
         return self.last_mouse_location[1]
 
+    @staticmethod
+    def calculate_gravity_acceleration(M, r):
+        return G*M/r**2
+
     def on_show_view(self):
-        arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color(arcade.color.BLACK)
 
         # Don't show the mouse cursor
         # self.window.set_mouse_visible(False)
@@ -171,7 +185,7 @@ class GameView(arcade.View):
         # Draw all the sprites.
         self.player_list.draw()
         self.coin_list.draw()
-        self.wall_list.draw()
+        self.planet_list.draw()
 
         # Select the (unscrolled) camera for our GUI
         self.camera_gui.use()
@@ -182,22 +196,29 @@ class GameView(arcade.View):
         output_total = f"Total Score: {self.window.total_score}"
         arcade.draw_text(output_total, 10, 10, arcade.color.WHITE, 14)
 
+        # draw mouse drag speed vector
         if self.mouse_pressed:
+            speed_vector = Vec2(self.mouse_x-self.last_mouse_x, self.mouse_y-self.last_mouse_y)
+            delta_v = speed_vector.mag
+            heading = math.degrees(speed_vector.heading)
             arcade.draw_line(start_x=self.last_mouse_x, start_y=self.last_mouse_y,
                              end_x=self.mouse_x, end_y=self.mouse_y,
-                             color=(0, 255, 255), line_width=5)
+                             color=arcade.color.ORANGE, line_width=min(5, math.ceil(delta_v/30)))
+            arcade.draw_text(f"∆v: {round(delta_v, 2)} m/s", self.mouse_x + 20, self.mouse_y, arcade.color.WHITE, 14)
+            arcade.draw_text(f"heading: {round(heading, 2)}°", self.mouse_x + 20, self.mouse_y+20, arcade.color.WHITE, 14)
+
 
     def on_update(self, delta_time):
         self.time_taken += delta_time
 
-        if self.up_pressed and not self.down_pressed:
-            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
-        elif self.down_pressed and not self.up_pressed:
-            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
-        if self.left_pressed and not self.right_pressed:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
-        elif self.right_pressed and not self.left_pressed:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+        # if self.up_pressed and not self.down_pressed:
+        #     self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        # elif self.down_pressed and not self.up_pressed:
+        #     self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        # if self.left_pressed and not self.right_pressed:
+        #     self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        # elif self.right_pressed and not self.left_pressed:
+        #     self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
         # Generate a list of all sprites that collided with the player.
         hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
@@ -218,6 +239,10 @@ class GameView(arcade.View):
             self.window.show_view(game_over_view)
 
         # Call update on all sprites
+        self.planet_list.update()
+        self.player_sprite.update()
+        self.coin_list.update()
+
         self.physics_engine.update()
 
         # Scroll the screen to the player
@@ -262,6 +287,10 @@ class GameView(arcade.View):
 
     def on_mouse_release(self, x, y, button, key_modifiers):
         self.mouse_pressed = False
+        speed_vector = Vec2(self.mouse_x - self.last_mouse_x, self.mouse_y - self.last_mouse_y)
+        # delta_v = speed_vector.mag
+        # heading = math.degrees(speed_vector.heading)
+        self.player_sprite.change_speed_vector += speed_vector.scale(PLAYER_MOVEMENT_SPEED)
 
     def scroll_to_player(self):
         """
